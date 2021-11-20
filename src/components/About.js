c79af965732b33f34f087df727c6c6f0803fb6be
Dico Diaz <dicodiaz@gmail.com>
2021-11-20 00:40:33 -0500

    Add latest version of react-router-dom and some Routes

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+const About = () => (
+  <main className="bg-dark text-white min-vh-100 d-flex flex-column justify-content-center">
+    <div className="container-md">
+      <div className="row mx-0 justify-content-center">
+        <div className="col-md-9 text-center">Coming soon</div>
+      </div>
+    </div>
+  </main>
+);
+
+export default About;
